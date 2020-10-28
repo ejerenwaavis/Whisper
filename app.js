8dcbbd868be18c18c0855923f257a1b027c85a0d
Avis Ejerenwa <ejerenwaavis@gmail.com>
2020-10-27 21:25:11 -0400

    Level 5 - Google Oauth Authentication

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-// require("dotenv").config();
-// let secrete = process.env.SECRETE;
+require("dotenv").config();
+const clientSecrete = process.env.CLIENT_SECRETE;
+const clientID = process.env.CLIENT_ID;
 
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 
 //Authentication & Session Management
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+// GoogleStrategy Authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -24,25 +30,45 @@ app.use(passport.session());
 
 // DATABASE CONNECTION
 // URI
-const uri = "mongodb+srv://Admin-Avis:Password123@db1.s2pl8.mongodb.net/auth-level-2";
+const uri = "mongodb+srv://Admin-Avis:Password123@db1.s2pl8.mongodb.net/auth-level-3";
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 // Tell the Schema to use the passportLocalMongoose plugin
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//Serialize implementation
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+//deserialize implementation
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
-
+//telling passport to use GoogleStrategy
+passport.use(new GoogleStrategy({
+    clientID: clientID,
+    clientSecret: clientSecrete,
+    callbackURL: "http://localhost:3000/auth/google/secretes",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //------- Home Route--------//
 app.route("/")
@@ -66,8 +92,14 @@ app.route("/secretes")
 //------- Login Route--------//
 app.route("/login")
 .get(function(req, res){
-  let body =  new Body("", "", "Login");
-  res.render("login", {body:body});
+  if(req.isAuthenticated()){
+    const body = new Body("","","Secretes")
+    body.message = "Session Active"
+    res.render("secretes", {body:body});
+  }else{
+    const body =  new Body("", "", "Login");
+    res.render("login", {body:body});
+  }
 })
 .post(function(req,res){
   const user = new User({
@@ -91,8 +123,14 @@ app.route("/login")
 //------- Register Route--------//
 app.route("/register")
 .get(function(req,res){
-  let body =  new Body("", "", "Register");
-  res.render("register", {body:body});
+  if(req.isAuthenticated()){
+    const body = new Body("","","Secretes")
+    body.message = "Session Active"
+    res.render("secretes", {body:body});
+  }else{
+    let body =  new Body("", "", "Register");
+    res.render("register", {body:body});
+  }
 })
 .post(function(req, res){
   const body = new Body("","Registered Successfully","Secret");
@@ -119,15 +157,23 @@ app.route("/logout")
 });
 
 
-// app.route("/secretes")
-// .get(function(req, res){
-//   let body = {
-//     error: "",
-//     message: "",
-//     title: "Secretes"
-//   }
-//   res.render("secretes", {body:body});
-// })
+
+app.route("/auth/google")
+.get(
+  passport.authenticate("google", {scope:["profile"]} )
+);
+
+
+app.route("/auth/google/secretes")
+.get(
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect user page.
+    console.log("Logged IN");
+    res.render('secretes', {body:new Body("","Google Authentication Successful", "Secretes")});
+  }
+);
+
 
 
 function Body(error, message, title){
@@ -138,4 +184,4 @@ function Body(error, message, title){
 
 app.listen(3000, function(){
   console.log("Whisper running on 3000");
-})
+});
